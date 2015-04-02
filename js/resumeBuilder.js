ab58e3007e992ed7999fbccc9c23be55b9c954dd
Rachel Lee Campbell <rachelleecampbell@gmail.com>
2015-04-01 22:46:16 -0500

    added header to js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,12 @@
+
+
+var name = "Rachel Trantham";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+ 
