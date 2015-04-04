f243ceaa497e3217116f62c14cf9f0a7be193f1d
Rachel Lee Campbell <rachelleecampbell@gmail.com>
2015-04-03 21:22:58 -0500

    Added bio using object literal notation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,4 +9,17 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
- 
+ var skills = 
+ ["graphic design", "programming", "sales"]
+
+ // $("#main").append(skills);
+
+var bio = {
+  "name" : "Rachel Trantham",
+  "role" : role,
+  "contact info" : "rachelleecampbell@gmail.com",
+  "welcome message" : "Welcome to my dynamic resume",
+  "skills" : skills
+};
+
+$("#main").append(bio.name);
